03cbd3430ad577de65f59e86bfa7717fc435a724
CHIN-HUA <tico9004040@gmail.com>
2021-05-29 21:20:22 +0800

    Create main.cpp

---
+++ K-I-S-S-I-N-G (一行版)c726/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string boy, girl;
+    cin >> girl >> boy;
+    cout << girl << " and " << boy << " sitting in the tree";
+    return 0;
+}
